1fc774b11695d449285ee6fcfcada5e5aed4f61c
Sharad Agarwal <sharad.agarwal@outlook.com>
2017-07-21 13:06:23 -0700

    new post topic unit test; some better commenting

--- EmbeddedSocialClient/EmbeddedSocialClientTests/EmbeddedSocialClientTests.swift
+++ EmbeddedSocialClient/EmbeddedSocialClientTests/EmbeddedSocialClientTests.swift
@@ -11,24 +11,27 @@ import XCTest
 
 class EmbeddedSocialClientTests: XCTestCase {
     
+    // how long to wait for async API calls to finish before giving up
     let testTimeout = 10.0
     
+    // Put setup code here. This method is called before the invocation of each test method in the class.
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
+    // Test the GetBuildsCurrent API call
     func testGetBuildsCurrent() {
         let expectation = self.expectation(description: "testGetBuildsCurrent")
         
-        // let appKey = "xx"
-        // EmbeddedSocialClientAPI.customHeaders = ["Authorization" : "Anon AK=\(appKey)"]
+        // setup the client interface
         EmbeddedSocialClientAPI.basePath = "https://ppe.embeddedsocial.microsoft.com"
+
+        // issue the call
         BuildsAPI.buildsGetBuildsCurrent { (buildsCurrentResponse: BuildsCurrentResponse?, error: Error?) in
             if let error = error {
                 XCTFail("error getting builds info : \(error.localizedDescription)")
@@ -43,6 +46,48 @@ class EmbeddedSocialClientTests: XCTestCase {
             XCTAssert(buildsCurrentResponse.commitHash != nil)
             XCTAssert(!buildsCurrentResponse.commitHash!.isEmpty)
             print("commit hash is \(buildsCurrentResponse.commitHash!)")
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: testTimeout) { error in
+            if let error = error {
+                XCTFail("waitForExpectations errored: \(error)")
+            }
+        }
+    }
+    
+    // Test the PostTopic API call
+    func testPostTopic() {
+        let expectation = self.expectation(description: "testPostTopic")
+        
+        // setup the client interface
+        let appKey = "XX"
+        let token = "XX"
+        EmbeddedSocialClientAPI.basePath = "https://ppe.embeddedsocial.microsoft.com"
+        EmbeddedSocialClientAPI.customHeaders = ["Authorization":"Google AK=\(appKey)|TK=\(token)"]
+        
+        // create a topic request
+        let postTopicRequest = PostTopicRequest()
+        postTopicRequest.title = "my new topic title"
+        postTopicRequest.text = "my new topic text"
+        postTopicRequest.blobType = PostTopicRequest.BlobType.unknown
+        postTopicRequest.publisherType = PostTopicRequest.PublisherType.user
+        
+        // issue the API call
+        TopicsAPI.topicsPostTopic(request: postTopicRequest) { (postTopicResponse: PostTopicResponse?, error: Error?) in
+            if let error = error {
+                XCTFail("error posting new topic : \(error.localizedDescription)")
+                return
+            }
+            
+            guard let postTopicResponse = postTopicResponse else {
+                XCTFail("did not get post topic response")
+                return
+            }
+            
+            XCTAssert(postTopicResponse.topicHandle != nil)
+            XCTAssert(!postTopicResponse.topicHandle!.isEmpty)
+            print("new topic handle is \(postTopicResponse.topicHandle!)")
             expectation.fulfill()
         }
         
